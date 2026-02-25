eaffbff4e2f417fc88eaa04d05b0a4f792c407d4
Ariyan <ariyan>
2026-02-25 12:56:25 +0530

    create the api for Subscriber Verification & Analytics

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile, NewsletterSlot, Notification, SwapRequest, Book
+from .models import Profile, NewsletterSlot, Notification, SwapRequest, Book, SubscriberVerification
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(NewsletterSlot)
 admin.site.register(Notification)
 admin.site.register(SwapRequest)
 admin.site.register(Book)
+admin.site.register(SubscriberVerification)
